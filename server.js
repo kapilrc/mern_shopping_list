054ce6d254fd0caf95bcad399ddfd2c30478bec1
kapilrc <kapilrc@gmail.com>
2019-06-07 20:01:42 +0530

    initial commit with mongodb connect

---
+++ server.js
@@ -0,0 +1,21 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const bodyParser = require("body-parser");
+
+const app = express();
+
+//BodyParser  MiddleWare
+app.use(bodyParser.json());
+
+// DB config
+const db = require('./config/keys').mongoURI;
+
+// connect to mongo
+mongoose
+    .connect(db)
+    .then(() =>  console.log("mongoDB connected..."))
+    .catch(err => console.log(err));
+
+const port = process.env.PORT || 5000;
+
+app.listen(port, () => console.log(`server started on port ${port}`));
